f8369fde69606b82f4e368b04947dcbc66964282
Lucas Fraga <ti.lucasfraga@gmail.com>
2019-06-08 23:25:49 -0300

    Responsive fix

--- assets/includes/modal-list.php
+++ assets/includes/modal-list.php
@@ -1,15 +1,17 @@
 <!-- Modal -->
 <div class="modal fade" id="modal-bitminer" role="dialog" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">BitMiner</h5>
+        <h5 class="modal-title">BitMiner</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <iframe id ="iframe-bitminer" src="https://bitminer.io/"></iframe>
+        <div class="embed-responsive embed-responsive-21by9">
+            <iframe id ="embed-responsive-item" src="https://bitminer.io/"></iframe>
+        </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -20,16 +22,18 @@
 
 <!-- Modal -->
 <div class="modal fade" id="modal-btcprominer" role="dialog" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">BTCProMiner</h5>
+        <h5 class="modal-title">BTCProMiner</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <iframe id ="iframe-btcprominer" src="https://btcprominer.life/"></iframe>
+        <div class="embed-responsive embed-responsive-21by9">
+          <iframe id ="embed-responsive-item" src="https://btcprominer.life/"></iframe>
+        </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -41,16 +45,18 @@
 
 <!-- Modal -->
 <div class="modal fade" id="modal-startminer" role="dialog" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">StartMiner</h5>
+        <h5 class="modal-title">StartMiner</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <iframe id ="iframe-startminer" src="https://www.startminer.com/"></iframe>
+        <div class="embed-responsive embed-responsive-21by9">
+          <iframe id ="embed-responsive-item" src="https://www.startminer.com/"></iframe>
+        </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
