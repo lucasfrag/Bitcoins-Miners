48ee1db40e791302c6280a18e7735f371dab1d81
Lucas Fraga <ti.lucasfraga@gmail.com>
2019-07-31 15:04:18 -0300

    Add new miners

--- assets/includes/modal-list.php
+++ assets/includes/modal-list.php
@@ -64,3 +64,87 @@
     </div>
   </div>
 </div>
+
+<div class="modal fade" id="modal-99mining" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">99mining</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="embed-responsive embed-responsive-21by9">
+          <iframe id ="embed-responsive-item" src="https://99mining.cloud/profile.php"></iframe>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="modal-jetmining" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">JET Mining</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="embed-responsive embed-responsive-21by9">
+          <iframe id ="embed-responsive-item" src="https://www.jetmining.website/dashboard"></iframe>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="modal-legitmining" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Legit Mining</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="embed-responsive embed-responsive-21by9">
+          <iframe id ="embed-responsive-item" src="https://www.legitmining.website/dashboard"></iframe>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="modal-btcmining" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">BTC Mining</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="embed-responsive embed-responsive-21by9">
+          <iframe id ="embed-responsive-item" src="https://miningfreebitcoin.com/dashboard.php"></iframe>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+  $json_mercadobitcoin = file_get_contents("https://www.mercadobitcoin.net/api/btc/ticker/");
+  $datamercadobitcoin = json_decode($json_mercadobitcoin, true);
+  $mercadobitcoin_price = $datamercadobitcoin['ticker']['last'];
+  $mercadobitcoin_volume = $datamercadobitcoin['ticker']['vol'];
+  //$mercadobitcoin_price = intval($mercadobitcoin_price);
+  //$mercadobitcoin_volume = intval($mercadobitcoin_volume);
+  $varmercadobitcoin = $mercadobitcoin_price * $mercadobitcoin_volume;
+?>
 <!DOCTYPE html>
 <html>
   <?php 
@@ -20,12 +29,18 @@
 
 				<div class="card-body">
 					<div class="row">
+						<div class="col-lg-12">
+								<h2 class="text-center">Valor atual do Bitcoin:</h2>
+								<h1 class="text-center" style="color: #007bff">R$ <?=number_format($mercadobitcoin_price, 2, ',', '.')?></h1>
+								<br>
+						</div>
+
 				        <div class="col-sm-6">
 						    <div class="card zoom-effect">
 						      <div class="card-body">
 						        <h5 class="card-title">BitMiner</h5>
 						        <p class="card-text" id="bitcoins-bitminer">
-						        	<img src="assets/img/bitminer.png" width="150px">
+						        	<img src="assets/img/bitminer.png" height="50px">
 						        </p>
 						        <a href="https://bitminer.io/" target="_blank" class="btn btn-secondary">Ir para página</a>
 						        <button class="btn btn-primary" data-toggle="modal" data-target="#modal-bitminer">Abrir Visualização</button>
@@ -38,7 +53,7 @@
 						      <div class="card-body">
 						        <h5 class="card-title">BTCProMiner</h5>
 						        <p class="card-text" id="bitcoins-bitminer">
-						        	<img src="assets/img/btcprominer.png" width="150px">
+						        	<img src="assets/img/btcprominer.png" height="50px">
 						        </p>
 						        <a href="https://btcprominer.life/" target="_blank" class="btn btn-secondary">Ir para página</a>
 						        <button class="btn btn-primary" data-toggle="modal" data-target="#modal-btcprominer">Abrir Visualização</button>
@@ -51,10 +66,62 @@
 						      <div class="card-body">
 						        <h5 class="card-title">StartMiner</h5>
 						        <p class="card-text" id="bitcoins-bitminer">
-						        	<img src="assets/img/startminer.png" width="150px">
+						        	<img src="assets/img/startminer.png" height="50px">
 						        </p>
 						        <a href="https://www.startminer.com/" target="_blank" class="btn btn-secondary">Ir para página</a>
 						        <button class="btn btn-primary" data-toggle="modal" data-target="#modal-startminer">Abrir Visualização</button>
+						      </div>
+						    </div>
+						</div>
+
+				        <div class="col-sm-6">
+						    <div class="card zoom-effect">
+						      <div class="card-body">
+						        <h5 class="card-title">99mining</h5>
+						        <p class="card-text" id="bitcoins-bitminer">
+						        	<img src="assets/img/99mining.png" height="50px">
+						        </p>
+						        <a href="https://99mining.cloud/profile.php" target="_blank" class="btn btn-secondary">Ir para página</a>
+						        <button class="btn btn-primary" data-toggle="modal" data-target="#modal-99mining">Abrir Visualização</button>
+						      </div>
+						    </div>
+						</div>
+
+				        <div class="col-sm-6">
+						    <div class="card zoom-effect">
+						      <div class="card-body">
+						        <h5 class="card-title">JET Mining</h5>
+						        <p class="card-text" id="bitcoins-bitminer">
+						        	<img src="assets/img/jetmining.png" height="50px">
+						        </p>
+						        <a href="https://www.jetmining.website/dashboard" target="_blank" class="btn btn-secondary">Ir para página</a>
+						        <button class="btn btn-primary" data-toggle="modal" data-target="#modal-jetmining">Abrir Visualização</button>
+						      </div>
+						    </div>
+						</div>
+
+				        <div class="col-sm-6">
+						    <div class="card zoom-effect">
+						      <div class="card-body">
+						        <h5 class="card-title">Legit Mining</h5>
+						        <p class="card-text" id="bitcoins-bitminer">
+						        	<img src="assets/img/legitmining.png" height="50px">
+						        </p>
+						        <a href="https://www.legitmining.website/dashboard" target="_blank" class="btn btn-secondary">Ir para página</a>
+						        <button class="btn btn-primary" data-toggle="modal" data-target="#modal-legitmining">Abrir Visualização</button>
+						      </div>
+						    </div>
+						</div>
+
+				        <div class="col-sm-6">
+						    <div class="card zoom-effect">
+						      <div class="card-body">
+						        <h5 class="card-title">BTC Mining</h5>
+						        <p class="card-text" id="bitcoins-bitminer">
+						        	<img src="assets/img/btcmining.png" height="50px">
+						        </p>
+						        <a href="https://miningfreebitcoin.com/dashboard.php" target="_blank" class="btn btn-secondary">Ir para página</a>
+						        <button class="btn btn-primary" data-toggle="modal" data-target="#modal-btcmining">Abrir Visualização</button>
 						      </div>
 						    </div>
 						</div>
